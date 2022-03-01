128bb52ea83ce55dc2f825c9c5aa491404920ff8
Baher Zakhary <baher.zakharyy@gmail.com>
2022-03-01 06:26:14 +0200

    Connect to mongodb

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ require('dotenv').config()
 var express = require('express');
 var app = express();
 
+const mongoose = require('mongoose');
+const URI = process.env.MONGO_LOCAL_DB_URI;
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.get('/', (req, res) => {
   console.log("Test mongodb trial");
 });
