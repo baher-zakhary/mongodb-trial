d47af8204ba2823bb170b6fe0e2d9a0372a5bb60
Baher Zakhary <baher.zakharyy@gmail.com>
2022-03-02 07:01:03 +0200

    - Create Schema - Create Model - Insert Model into database - Insert many models into database

--- myApp.js
+++ myApp.js
@@ -7,39 +7,41 @@ const mongoose = require('mongoose');
 const URI = process.env.MONGO_LOCAL_DB_URI;
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const personSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model("Person", personSchema);
+
+const Lilly = new Person({
+  name: "Lilly",
+  age: 30,
+  favoriteFoods: ["Pizza", "Pasta"]
+});
+
+const createAndSavePerson = (person, done) => {
+  person.save((err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      done(null, data);
+    }
+  });
+};
+
+const createManyPeople = (arrayOfPeople, done) => {
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) {
+      console.error(err);
+    }
+    done(null, data)
+  });
+};
+
 app.get('/', (req, res) => {
   console.log("Test mongodb trial");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = app;
